bb757ed087dea357e9ad608e36662efe3b77017a
SDEEY <75689630+SDEEY@users.noreply.github.com>
2022-09-20 04:43:26 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,10 +6,10 @@ import OwnLayout from "./OwnLayout/OwnLayout";
 import imgDiscord from './icons8-discord-50.png'
 import imgTwitter from './icons8-twitter-50.png'
 
-const solAmount = 0.3
-const image = 'https://static9.depositphotos.com/1307373/1179/i/600/depositphotos_11794280-stock-photo-red-apple.jpg'
-const Title = 'Dragon Elements'
-const supply = 1110
+const solAmount = 0
+const image = 'https://pbs.twimg.com/profile_images/1571259706747805696/Qa6AL6IX_400x400.jpg'
+const Title = 'Ballon Degenz'
+const supply = 3333
 
 document.title = Title
 document.getElementById('favicon').setAttribute('href', image)
