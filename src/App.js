1b691f065413364986993829d483c6f432cf3834
SDEEY <75689630+SDEEY@users.noreply.github.com>
2023-05-03 22:45:38 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,10 +6,10 @@ import OwnLayout from "./OwnLayout/OwnLayout";
 import imgDiscord from './icons8-discord-50.png'
 import imgTwitter from './icons8-twitter-50.png'
 
-const solAmount = "0"
-const image = 'https://pbs.twimg.com/profile_images/1645819873220591616/zBPhKkps_400x400.jpg'
-const Title = 'visages'
-const supply = 333
+const solAmount = "1.75"
+const image = 'https://pbs.twimg.com/profile_images/1630667975605862400/8MZhYv57_400x400.jpg'
+const Title = 'Fox Capital'
+const supply = 101
 
 document.title = Title
 document.getElementById('favicon').setAttribute('href', image)
