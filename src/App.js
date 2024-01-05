ad5f0d1079a3eead6c89de8f78e0c447fd0d0921
SDEEY <75689630+SDEEY@users.noreply.github.com>
2024-01-05 22:25:44 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,10 +6,10 @@ import OwnLayout from "./OwnLayout/OwnLayout";
 import imgDiscord from './icons8-discord-50.png'
 import imgTwitter from './icons8-twitter-50.png'
 
-const solAmount = '0.19'
-const image = 'https://pbs.twimg.com/profile_images/1705255078792454144/OWQSQ4-q_400x400.jpg'
-const Title = 'Just Art'
-const supply = 80
+const solAmount = '0.15'
+const image = 'https://pbs.twimg.com/profile_images/1656328676768620549/ynTCpCiz_400x400.jpg'
+const Title = 'Elementals'
+const supply = 2844
 
 document.title = Title
 document.getElementById('favicon').setAttribute('href', image)
