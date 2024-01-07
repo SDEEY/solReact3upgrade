568fa7ed4d577a21a7d4fab35e410ce9c7c085a8
SDEEY <75689630+SDEEY@users.noreply.github.com>
2024-01-07 15:43:35 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,10 +6,10 @@ import OwnLayout from "./OwnLayout/OwnLayout";
 import imgDiscord from './icons8-discord-50.png'
 import imgTwitter from './icons8-twitter-50.png'
 
-const solAmount = '0.1'
-const image = 'https://pbs.twimg.com/profile_images/1738318317637586944/XDSQ4KCZ_400x400.jpg'
-const Title = 'bonklet nft'
-const supply = 3499
+const solAmount = '0.068 – 0.1'
+const image = 'https://pbs.twimg.com/profile_images/1737463329289867264/uVW-foU1_400x400.png'
+const Title = 'EpicZoo'
+const supply = 2222
 
 document.title = Title
 document.getElementById('favicon').setAttribute('href', image)
